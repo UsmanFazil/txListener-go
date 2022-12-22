36d17becb4533d505b366c62b7427d1cc3135b61
saadbutt <buttsaad44@gmail.com>
2022-12-22 14:07:29 +0500

    signature service updated

--- services/signature.go
+++ services/signature.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 
 	"github.com/block-listener/utils"
 	"github.com/ethereum/go-ethereum/common/math"
@@ -15,13 +16,20 @@ import (
 var pk = keyGen(utils.GetPK())
 
 func Sign(originChainId, toChainId int64, contractAddress, refId, msgSender, amount string, key *ecdsa.PrivateKey) []byte {
+	num := new(big.Int)
+	num, ok := num.SetString(amount, 10)
+	if !ok {
+		fmt.Println("SetString: error")
+	}
+
 	// Turn the message into a 32-byte hash
-	hash := solsha3.SoliditySHA3(
-		[]string{"uint256", "uint256", "address", "bytes32", "address", "uint256"},
-		[]interface{}{
-			originChainId, toChainId, contractAddress, refId, msgSender, amount,
-		},
-	)
+	hash := solsha3.SoliditySHA3(solsha3.Uint256(big.NewInt(originChainId)),
+		solsha3.Uint256(big.NewInt(toChainId)),
+		solsha3.Address(contractAddress),
+		solsha3.Bytes32(refId),
+		solsha3.Address(msgSender),
+		solsha3.Uint256(num))
+
 	// Prefix and then hash to mimic behavior of eth_sign
 	prefixed := solsha3.SoliditySHA3(solsha3.String("\x19Ethereum Signed Message:\n32"), solsha3.Bytes32(hash))
 	signature, err := secp256k1.Sign(prefixed, math.PaddedBigBytes(key.D, 32))
@@ -56,5 +64,13 @@ func keyGen(pkString string) *ecdsa.PrivateKey {
 		panic(err)
 	}
 
+	return privateKey
+}
+func KeyGen() *ecdsa.PrivateKey {
+	privateKey, err := crypto.HexToECDSA("7cd308f8b5f80233b279079ccf61dfc8ad4b2a7728ed971c2321794621f1f15c")
+
+	if err != nil {
+		panic(err)
+	}
 	return privateKey
 }
